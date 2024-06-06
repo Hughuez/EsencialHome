574048aee7594a60e494db82aca4df723267905d
Hughuez Andrés Ocampo <hughuez.and@gmail.com>
2024-06-06 00:05:23 -0500

    CRUD productos

--- back/controllers/productsController.js
+++ back/controllers/productsController.js
@@ -1,11 +1,57 @@
 const producto = require('../models/productos');
+const fetch = (url) => import('node-fetch').then(({default: fetch}) => fetch(url)); //usurpacion del require
 
 //Ver lista de productos
-exports.getProducts = (req, res, next) => {
+exports.getProducts = async (req, res, next) => {
+    const productos = await producto.find();
+
     res.status(200).json({
         success: true,
-        message: 'En esta ruta apareceran todolos los productos'
+        count: productos.length,
+        productos
     })
+}
+
+//ver producto por id
+exports.getProductById = async (req, res, next) => {
+    const product = await producto.findById(req.params.id);
+    if(!product){
+        return res.status(404).json({
+            success: false,
+            message: `producto id: ${req.params.id}, no encontrado` 
+        })
+    }else{
+        res.status(200).json({
+            success: true,
+            message: '¡producto encontrado!:',
+            product
+        })
+    }
+}
+
+// actualizar producto (update)
+exports.updateProduct = async (req, res, nex) => {
+    let product = await producto.findById(req.params.id); 
+    // verificamos existencia del objeto, si no existe termina el proceso
+    if(!product){
+        return res.status(404).json({
+            success: false,
+            message: `producto id: ${req.params.id}, no encontrado` 
+        })
+    }else{ // Encontrado el objeto hacemos actualizacion
+        product = await producto.findByIdAndUpdate(req.params.id, req.body, {
+            // Actualizamos solo los datos nuevos
+            new:true,
+            runValidators:true
+        })
+
+        // confirmo ok proceso de actualizacion
+        res.status(200).json({
+            success:true,
+            message: `¡${product.nombre} actualizado correctamente!`,
+            product
+        })
+    }
 }
 
 //crear un nuevo producto
@@ -17,3 +63,52 @@ exports.newProduct = async(req, res, next) => {
         product
     });
 }
+
+// eliminar producto
+exports.deleteProduct = async (req, res, nex) => {
+    const product = await producto.findById(req.params.id); 
+    if(!product){
+        return res.status(404).json({
+            success: false,
+            message: `producto id: ${req.params.id}, no encontrado` 
+        })
+    }else{ 
+        await product.deleteOne({ _id: req.params.id });
+
+        res.status(200).json({
+            success:true,
+            message:'Producto eliminado correctamente.'
+        })
+    }
+}
+
+/*
+FETCH
+-Es un recurso que nos ayuda a tener acceso a cierta informacion
+-Es un modulo, por lo que para usarlo debemos instalarlo
+-> no se puede llamar como un tipo objeto, por lo que hay que modificarlo USURPANDO EL REQUIRE para poder importarlo: const fetch = (url) => import('node-fetch').then(({default: fetch}) => fetch(url));
+- ventaja: 
+(1) no necesito una herramienta como postman para acceder a informacion
+(2) puedo guardar la informacino en una variable local para despues usarla
+(3) puedo mostrarla en consola
+*/
+
+// ver todos los productos con fetch (Metodo)
+function verProductos(){
+    fetch('http://localhost:4000/api/productos')
+    .then(res => res.json())
+    .then(res => console.log(res))
+    .catch(err => console.error(err))
+}
+
+// verProductos();
+
+// ver productos por id con fetch
+function verProductoPorId (id) {
+    fetch('http://localhost:4000/api/producto/'+id)
+    .then(res => res.json())
+    .then(res => console.log(res))
+    .catch(err => console.error(err))
+}
+
+// verProductoPorId('665fe704d34ed60007c4b252');
--- back/routes/products.js
+++ back/routes/products.js
@@ -1,9 +1,19 @@
 const express = require('express');
 const router = express.Router();
 
-const {getProducts, newProduct} = require('../controllers/productsController') //traemos la respuesta json desde el controlador
+const {
+    getProducts, 
+    newProduct, 
+    getProductById,
+    updateProduct,
+    deleteProduct
+} = require('../controllers/productsController') //traemos la respuesta json desde el controlador
 
-router.route('/productos').get(getProducts) //Establecemos desde que ruta queremos ver getProducts
-router.route('/producto/nuevo').post(newProduct) // Establecemos ruta para crear producto nuevo
+// rutas
+router.route('/productos').get(getProducts); // Establecemos desde que ruta queremos ver getProducts
+router.route('/producto/nuevo').post(newProduct); // Establecemos ruta para crear producto nuevo
+router.route('/producto/:id').get(getProductById); // consulta por id
+router.route('/producto/:id').put(updateProduct); // actualizacion de producto (update)
+router.route('/producto/:id').delete(deleteProduct); // eliminar producto por id
 
 module.exports = router;
