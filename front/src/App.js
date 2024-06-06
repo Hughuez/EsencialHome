934edc36626326f41b282c87c1dd062678694c7a
Hughuez Andrés Ocampo <hughuez.and@gmail.com>
2024-06-06 14:58:15 -0500

    commit front react

---
+++ front/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Esencial Home Store</h1>
+      <img src= "./images/esencialHome.jpg" alt='imagen Esencial Home' ></img>
+    </div>
+  );
+}
+
+export default App;
