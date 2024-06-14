8fc3f233879de677ff4e01acfde23348b9abbd04
Hughuez Andrés Ocampo <hughuez.and@gmail.com>
2024-06-13 23:34:14 -0500

    dashboard admin, lista productos y producto nuevo

--- front/src/App.js
+++ front/src/App.js
@@ -5,6 +5,9 @@ import Footer from './components/layaout/Footer';
 import Home from './components/Home';
 import { ProductDetails } from './components/products/ProductDetails';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Dashboard from './components/admin/Dashboard';
+import ProductsList from './components/admin/ProductsList';
+import NewProduct from './components/admin/newProduct';
 
 function App() {
   return (
@@ -16,6 +19,9 @@ function App() {
             <Route path='/home' element={<Home />} />
             <Route path='/' element={<Home />} />
             <Route path="/producto/:id" element={<ProductDetails />}/>
+            <Route path="/Dashboard" element={<Dashboard />}/> 
+            <Route path="/productList" element={<ProductsList />}/>
+            <Route path="/nuevoProducto" element={<NewProduct />}/>
           </Routes>
         </div>
         
--- front/src/components/layaout/Header.js
+++ front/src/components/layaout/Header.js
@@ -1,4 +1,6 @@
-import React, { Fragment } from 'react'
+import React, { Fragment } from 'react';
+import "../../App.css"
+import {Link} from "react-router-dom"
 
 const Header = () => {
   return (
@@ -27,8 +29,17 @@ const Header = () => {
             </div>
             
             <div className='col-12 col-md-3 mt-4 mt-md-0 tex-center'>
-                <span><button type='button' class='btn btn-warning'>Ingresar</button></span>
-                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <div className="ml-4 dropdown d-inline">
+                        <Link to="#!" className="btn dropdown-toggle mr-4" type="button"
+                        id="dropDownMenu" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            <span>Menú</span></Link>&nbsp;&nbsp;&nbsp;&nbsp;  
+                            <div className='dropdown-menu' aria-labelledby='dropDownMenu'>
+                                <Link className="dropdown-item" to="/Dashboard">Adm. Productos</Link>
+                                <Link className="dropdown-item" to="/">Pedidos</Link>
+                                <Link className="dropdown-item" to="/">Mi cuenta</Link>
+                                <Link className="dropdown-item" to="/">Cerrar Sesion</Link>
+                            </div>
+                    </div>
                 <i class='fa fa-shopping-cart fa-2x' aria-hidden='true'></i>
                 <span className='ml-1' id='cart_count'>2</span>
             </div>
