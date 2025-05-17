ee5e8bf5dda6729873d47e4bec6aebf3e448ac78
Hughuez Andrés Ocampo <hughuez.and@gmail.com>
2025-05-17 14:16:18 -0500

    actualización fecha
    cambio fecha footer

--- front/src/components/layaout/Footer.js
+++ front/src/components/layaout/Footer.js
@@ -5,7 +5,7 @@ export const Footer = () => {
     <Fragment>
         <footer className='py-1'>
             <p  className='text-center mt-1'>
-                Esencial Hogar - un producto de HZ - Todos los derechos reservados 2024®
+                Esencial Hogar - un producto de HZ - Todos los derechos reservados 2025®
             </p>
         </footer>
     </Fragment>
